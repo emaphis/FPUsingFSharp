8843be2349482ac9018f60f9e16d039fa7216627
Ed Maphis <emaphis85@gmail.com>
2020-04-16 21:35:12 -0400

    Capstone 1

---
+++ lesson-08/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open Car
+
+let getDestination() =
+    Console.Write("Enter destination: ")
+    Console.ReadLine()
+
+let mutable petrol = 100;
+
+[<EntryPoint>]
+let main argv =
+    while true do
+        try
+            let destination = getDestination()
+            printfn "Trying to drive to %d" destination
+            petrol <- driveTo(petrol, destination)
+            printfn "Made it to %s! You have %d petrol left" destination petrol
+        with ex -> printfn "Error: %s" ex.Message
+    0
