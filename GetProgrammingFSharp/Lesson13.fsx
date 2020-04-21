121c760c68313449289971be8aca46bc1f8f48b3
Ed Maphis <emaphis85@gmail.com>
2020-04-21 14:04:46 -0400

    Lesson 13 - Code reuse with higher order functions

---
+++ GetProgrammingFSharp/Lesson13.fsx
@@ -0,0 +1,57 @@
+﻿//// Lesson 13 - Achieving code reuse in F#
+
+// Reusing code with lambdas
+
+(* Quick check 13.1
+1 Name one difference between nominal and structural types.
+A- nominal type a typed by names, structural types are differentiated by components
+
+2 How do we pass logic between or across code in the OO world?
+A- You pass logic with interfaces and classed implementing that interface.
+
+3 How do we pass logic between or across code in the FP world?
+A- Lamba (unnamed) functions
+*)
+
+/// 13.2 Implementing higher-order functions in F#
+
+/// 13.2.1 Basics of higher-order functions
+
+/// Now you try
+
+/// Listing 13.5 - Your first higher-order function in F#
+type Customer =
+    { Age : int }
+
+// function to factor
+let whereCustomersAreOver35 customers =
+    seq {
+        for customer in customers do
+            if customer.Age > 35 then  // hard coded filter
+                yield customer }
+
+let where filter customers =
+    seq {
+        for customer in customers do
+            if filter customer then
+                yield customer }
+
+let isOver35 customer = customer.Age > 35
+
+let customers = [ { Age = 21 }; { Age = 35}; { Age = 36} ]
+
+customers |> where isOver35
+customers |> where (fun customer -> customer.Age > 35)
+
+
+(* Quick check 13.2
+1 Can F# infer the types of higher-order functions?
+A- Yes
+2 How can you easily identify higher-order function arguments in VS?
+A- 
+*)
+
+
+/// 13.3 Dependencies as functions
+
+// See Lesson13Try.fsx
---
+++ GetProgrammingFSharp/Lesson13Try.fsx
@@ -0,0 +1,47 @@
+﻿//// Lesson 13 try
+
+/// Listing 13.6 - Injecting dependencies into functions
+
+open System
+
+type Customer =
+    { Age : int }
+
+// writer is first parameter for partial application
+let printCustmerAge writer customer =
+    if customer.Age < 13 then writer "Child"
+    elif customer.Age < 20 then writer "Teenager!"
+    else writer "Adult!"
+
+/// Listing 13.7 Partially applying a function with dependencies
+
+let cust = { Age = 25 }
+
+printCustmerAge Console.WriteLine cust
+printCustmerAge (fun str -> str.Length) cust
+
+printCustmerAge Console.WriteLine { Age = 23 }
+
+let printToConsole = printCustmerAge Console.WriteLine
+printToConsole { Age = 21 }
+printToConsole { Age = 12 }
+printToConsole { Age = 18 }
+
+
+/// Listing 13.8 Creating a dependency to write to a file
+open System.IO
+
+let path = @"C:\bin\output.txt"
+let writeToFile text = File.WriteAllText(path, text)
+
+let printToFile = printCustmerAge writeToFile
+printToFile { Age = 21 }
+
+let text = File.ReadAllText(path)
+text = "Adult!"
+
+(* Quick check 13.3
+What’s the key difference between passing dependencies in F# and C#?
+A- F# pases fuctions of the right signature
+   C# passed classes implementing interfaces.
+*)
