ceeabe6353e44d3013e16cb9d8c4b4ffc7c75db6
Ed Maphis <emaphis85@gmail.com>
2020-05-06 11:41:31 -0400

    Lesson 27 - Exposing F# types and functions to C#

---
+++ lesson-27/CSharpApp/Program.cs
@@ -0,0 +1,39 @@
+﻿//// Lesson 27 - Exposing F# types and functions to C# - pg 321
+using Model;
+using System;
+using Microsoft.FSharp.Core;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Supacars", Tuple.Create(1.5, 3.5));
+            var wheeles = car.Wheels;
+
+            var bike = Vehicle.NewMotorbike("Harley", 4.0);
+            var car2 = Vehicle.NewMotorcar(car);
+
+            Console.WriteLine("A car: {0}", car);
+            Console.WriteLine("A bike: {0}", bike);
+
+            Functions.Describe(bike);
+            Functions.Describe(car2);
+
+            var car3 = Functions.CreateCar(4, "Chevy", 1.5, 3.5);
+            var car4 =
+                Functions.CreateFourWheeledCar
+                    .Invoke("Chevy")
+                    .Invoke(1.5)
+                    .Invoke(3.5);
+
+            // Woring with Options
+            var optionalCar = FSharpOption<Car>.Some(car);
+            var isNone = FSharpOption<Car>.get_IsNone(car);
+            var isNotSome = FSharpOption<Car>.get_IsSome(optionalCar);
+            Console.WriteLine("{0} : {1} : {2}", optionalCar, isNone, isNotSome);
+
+        }
+    }
+}
---
+++ lesson-27/FSharpCode/Library.fs
@@ -0,0 +1,39 @@
+﻿//// Lesson 27 - Exposing F# types and functions to C# - pg 321
+
+/// Listing 27.1 An F# record to be accessed from C#
+namespace Model
+
+/// A standard F# record of a Car.
+type Car =
+    { /// The number of wheels on the car.
+      Wheels : int
+      
+      /// The brand of the car.
+      Brand : string
+      
+      /// The x/y demensions of the car in meters
+      Dimensions : float * float }
+
+/// A vehicle of sone sort
+type Vehicle =
+
+/// A car is a type of vehicle.
+| Motorcar of Car
+
+/// A bike is also a type of vehicle
+| Motorbike of Name:string * EngineSize:float
+
+/// Listing 27.3 Exposing a module of functions to C# - pg 327
+module Functions =
+    /// Creates a car
+    let CreateCar wheels brand x y =
+        { Wheels = wheels; Brand = brand; Dimensions = x, y }
+
+    /// Creates a car with four wheels
+    let CreateFourWheeledCar = CreateCar 4
+
+    /// Describes a vehicle
+    let Describe vehicle =
+        match vehicle with
+        | Motorcar c -> printfn "This is a car with %d wheels!" c.Wheels
+        | Motorbike(_, size) -> printfn "This is a bike wiht engine %f" size
